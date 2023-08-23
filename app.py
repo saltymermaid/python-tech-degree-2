e59ac72467d7abdf9fe68f6ce4225e2ae296b93b
Melissa Blades <melissa@saltymermaid.dev>
2023-08-23 10:15:14 -0400

    Add basic functionality

--- app.py
+++ app.py
@@ -37,10 +37,8 @@ def convert_guardians(guardians_str):
 
 def pick_smallest_team(teams):
     """Pick one of the smallest teams"""
-    team_dict = {team['name']: len(team['players']) for team in teams}
-    smallest_size = min(team_dict.values())
-    smallest_team_name = [key for (key, value) in team_dict.items() if value == smallest_size][0]
-    smallest_team = [team for team in teams if team['name'] == smallest_team_name][0]
+    smallest_size = min(team['size'] for team in teams)
+    smallest_team = [team for team in teams if team['size'] == smallest_size][0]
     return smallest_team
 
 
@@ -55,19 +53,84 @@ def balance_teams(team_names, players):
     return teams
 
 
-if __name__ == "__main__":
-    players = clean_data()
-    team_names = copy.deepcopy(constants.TEAMS)
-    assigned_teams = balance_teams(team_names, players)
-    player_stats = True
+def print_team_stats(team):
+    """Print the team stats"""
+    print()
+    team_head = "Team " + team['name'] + " Stats"
+    print("-" * len(team_head))
+    print(team_head)
+    print("-" * len(team_head))
+    print(f"Total players: {team['size']}")
+    print(f"Average height: {avg_height(team)} inches\n")
+    print("Players on Team:")
+    players_list = [player['name'] for player in team['players']]
+    players_str = ", ".join(players_list)
+    print(players_str)
+    print()
+    print("Guardians:")
+    guardians_list = [guardian for player in team['players'] for guardian in player['guardians']]
+    guardians_str = ", ".join(guardians_list)
+    print(guardians_str)
+
+
+def avg_height(team):
+    """Calculate the average height of a team"""
+    total_height = sum([player['height'] for player in team['players']])
+    avg_height = total_height / team['size']
+    avg_height = round(avg_height, 1)
+    return avg_height
+
+
+def print_header():
+    """Print the header"""
+    print()
+    print("*" * 30)
     print("BASKETBALL TEAM STATS TOOL\n")
+
+
+def print_menu():
+    """Print the menu"""
+    print()
     print("---- MENU ----\n")
+    print("A) Display Team Stats")
+    print("B) Quit\n")
+
+
+def choose_team(teams):
+    for index, team in enumerate(teams, 1):
+        print(f"{index}) Display Team {team['name']} Stats")
+    choice = input("Pick a team > ")
+    try:
+        choice = int(choice)
+        if choice < 1 or choice > len(teams):
+            raise ValueError
+    except ValueError:
+        print("Please enter a valid option")
+        return choose_team(teams)
+    else:
+        return teams[choice - 1]
+
+
+def menu(teams):
+    """Run the menu"""
+    player_stats = True
     while player_stats:
-        user_choice = input("Menu:\n A) Display Team Stats\n B) Quit\n\nEnter an option > ")
+        print_menu()
+        user_choice = input("Enter an option > ")
         if user_choice.upper() == 'A':
-            for team in assigned_teams:
-                print(team)
+            selected_team = choose_team(teams)
+            print_team_stats(selected_team)
+            print()
+            print("*" * 30)
         elif user_choice.upper() == 'B':
             player_stats = False
         else:
             print("Please enter a valid option")
+
+
+if __name__ == "__main__":
+    players = clean_data()
+    team_names = copy.deepcopy(constants.TEAMS)
+    assigned_teams = balance_teams(team_names, players)
+    print_header()
+    menu(assigned_teams)
